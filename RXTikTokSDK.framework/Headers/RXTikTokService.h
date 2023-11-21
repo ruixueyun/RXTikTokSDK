5fad8a8b89e47aceb8944e8e28dea087a856e7ad
chenhan <chenhan@jixiang.cn>
2023-11-21 09:21:26 +0800

    1.0.3

--- RXTikTokSDK.framework/Headers/RXTikTokService.h
+++ RXTikTokSDK.framework/Headers/RXTikTokService.h
@@ -7,6 +7,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <RXSDK_Pure/RXSDK_Pure.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -49,7 +50,7 @@ typedef NS_ENUM(NSUInteger, RXTTShareTypeLandedPageType) {
  * @param type 分享类型
  * @param landedPageType 在TikTok中的状态
  * @param localIdentifiers 分享资源
- * 您的应用程序在**相册**中向开放平台共享的视频或图像的本地标识符。内容必须是所有图像或视频。
+ * 您的应用程序在相册中向开放平台共享的视频或图像的本地标识符。内容必须是所有图像或视频。
  * 图像或视频的宽高比应在:[1/2.2,2.2]之间
  * 当“mediaType”为“Image”时:
  * 图像数量应多于1，最多为12。
@@ -62,6 +63,27 @@ typedef NS_ENUM(NSUInteger, RXTTShareTypeLandedPageType) {
            landedPageType:(RXTTShareTypeLandedPageType)landedPageType
          localIdentifiers:(NSArray *)localIdentifiers
                  complete:(void(^)(NSDictionary *response, NSDictionary *error))complete;
+
+/**
+ * TikTok 分享
+ * @note 首次调用会拉起授权弹框
+ * @note 使用该功能需要在 info.plist 中添加 Privacy - Photo Library Usage Description 相册权限说明
+ * @param type 分享类型
+ * @param landedPageType 在TikTok中的状态
+ * @param medias 分享资源，支持 url、本地路径
+ * 内容必须是图像或视频。
+ * 图像或视频的宽高比应在:[1/2.2,2.2]之间
+ * 当“mediaType”为“Image”时:
+ * 图像数量应多于1，最多为12。
+ * 当“mediaType”为“Video”时:
+ * 总视频时长应大于3秒。
+ * 共享视频不超过12个
+ * 带有品牌标志或水印的视频将导致视频被删除或帐户被禁。确保您的应用程序共享没有水印的内容。
+ */
+- (void)shareWithType:(RXTTShareType)type
+       landedPageType:(RXTTShareTypeLandedPageType)landedPageType
+               medias:(NSArray *)medias
+             complete:(RequestComplete)complete;
 
 @end
 
--- RXTikTokSDK.podspec
+++ RXTikTokSDK.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'RXTikTokSDK'
-  s.version          = '1.0.2'
+  s.version          = '1.0.3'
   s.summary          = 'ruixue RXLineSDK framework.'
 
 # This description is used to generate tags and improve searc results.
